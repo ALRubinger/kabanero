03fb6a6b10cf9b630b910ac54eb9a5b468df0c2d
Andrew Lohr <ajlohr@us.ibm.com>
2019-05-09 17:29:14 -0400

    fix filtering on stacks page

--- src/main/content/_assets/js/stacks.js
+++ src/main/content/_assets/js/stacks.js
@@ -1,43 +1,23 @@
+$( document ).ready(function() {
+    $(".filter").on("change", filterStacks);
+});
+
 function filterStacks(){
-    var filterCriteria = [];
-    filterCriteria.push($(".languageCheck:checked"));
-    filterCriteria.push($(".platformCheck:checked"));
-    filterCriteria.push($(".toolsCheck:checked"));
-    var tiles = $(".stackTile");
-    for(var tilesIndex = 0; tilesIndex < tiles.length; tilesIndex++){
-        $(tiles[tilesIndex]).show();
-        filterTile(tiles[tilesIndex], filterCriteria);
+    let selectedTags = $(".filter:checkbox:checked").map((index,filter) => $(filter).val()).get();
+
+    // if no filter is selected show all tiles
+    if(selectedTags.length === 0){
+        $(".stackTile").each((index, tile) => {
+            $(tile).show();
+        });
+        return;
     }
+
+    $(".stackTile").each((index, tile) => {
+        let currentTileTags = $(tile).data("tags").split(",");
+
+        // filter out the tiles with tags that aren't included in the selected tag filters
+        let tagMatches = currentTileTags.filter(tag => selectedTags.includes(tag.trim()));
+        tagMatches.length > 0 ? $(tile).show() : $(tile).hide();
+    });
 }
-
-function filterTile(tile, filterCriteria){
-    var langFilter = filterCriteria[0];
-    var platformFilter = filterCriteria[1];
-    var toolsFilter = filterCriteria[2];
-
-    if(langFilter.length > 0){
-        var tileLanguages = $(tile).data('language');
-        for(var langIndex = 0; langIndex < langFilter.length; langIndex++){
-            if(tileLanguages.indexOf($(langFilter[langIndex]).val()) === -1){
-                $(tile).hide();
-            }
-        }
-    }
-    if(platformFilter.length > 0){
-        var tilePlatforms = $(tile).data('platform');
-        for(var platIndex = 0; platIndex < platformFilter.length; platIndex++){
-            if(tilePlatforms.indexOf($(platformFilter[platIndex]).val()) === -1){
-                $(tile).hide();
-            }
-        }
-    }
-    if(toolsFilter.length > 0){
-        var tileTools = $(tile).data('tools');
-        for(var toolsIndex = 0; toolsIndex < toolsFilter.length; toolsIndex++){
-            if(tileTools.indexOf($(toolsFilter[toolsIndex]).val()) === -1){
-                $(tile).hide();
-            }
-        }
-    }
-}
-
