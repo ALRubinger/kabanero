82c5ca20c1f62d3fbe755434863dae1a9b97b918
Kevin See <kevinsee@kevins-mbp-2.raleigh.ibm.com>
2019-05-13 12:10:46 -0400

    removed unneeded functions

--- src/main/content/_assets/js/home.js
+++ src/main/content/_assets/js/home.js
@@ -1,11 +1,3 @@
-function showImgText(element){
-    $(element).next().show();
-}
-
-function hideImgText(element){
-    $(element).next().hide();
-}
-
 function hideAllFeaturedModelsCollapses(element){
     $(".featuredModelsCollapse").collapse('hide');
     $(element).collapse('show');
