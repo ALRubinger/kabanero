36f430f5ee7c2a147f70210838f040a10fe981a9
Andrew Lohr <ajlohr@us.ibm.com>
2019-05-15 13:24:10 -0400

    use show hide

--- src/main/content/_assets/js/home.js
+++ src/main/content/_assets/js/home.js
@@ -4,8 +4,8 @@ function hideAllFeaturedModelsCollapses(element){
 }
 
 function hideAllOpenSourcePlatformsCollapses(id){
-    $(".open_source_platform_content_box").collapse('hide');
-    $("#open_source_platform_" + id + "_collapse").collapse('show');
+    $(".open_source_platform_content_box").hide();
+    $("#open_source_platform_" + id + "_collapse").show();
     $(".open_source_platform_box_visible").removeClass("open_source_platform_box_visible");
     $("#open_source_platform_" + id + "_box").addClass("open_source_platform_box_visible");
     $(".open_source_platform_vertical_separator_visible").removeClass("open_source_platform_vertical_separator_visible");
