65086e3fb2eb2baa8592a57a9a8ed8a07203e05f
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-05-16 09:48:13 -0400

    merging master

--- src/main/content/_assets/js/home.js
+++ src/main/content/_assets/js/home.js
@@ -2,3 +2,12 @@ function hideAllFeaturedModelsCollapses(element){
     $(".featuredModelsCollapse").collapse('hide');
     $(element).collapse('show');
 }
+
+function hideAllOpenSourcePlatformsCollapses(id){
+    $(".open_source_platform_content_box").hide();
+    $("#open_source_platform_" + id + "_collapse").show();
+    $(".open_source_platform_box_selected").removeClass("open_source_platform_box_selected");
+    $("#open_source_platform_" + id + "_box").addClass("open_source_platform_box_selected");
+    $(".open_source_platform_vertical_separator_visible").removeClass("open_source_platform_vertical_separator_visible");
+    $("#open_source_platform_" + id + "_separator").addClass("open_source_platform_vertical_separator_visible");
+}
